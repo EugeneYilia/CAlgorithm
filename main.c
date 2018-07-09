2c9ca561dab18b8a757904ba38cae30009867307
root <root@DESKTOP-NM1J0DE.localdomain>
2018-07-09 13:03:25 +0800

    finish c algorithm1

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main() {//ÉıĞòÅÅĞò
+    int numbers[10];
+    printf("ÇëÊäÈëÊ®¸öÊı×Ö\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        int minimalIndex = i;
+        for (int j = i + 1; j < 10; j++) {
+            if (numbers[minimalIndex] > numbers[j]) {
+                minimalIndex = j;
+            }
+        }
+        if (minimalIndex != i) {
+            int tempNumber = numbers[i];
+            numbers[i] = numbers[minimalIndex];
+            numbers[minimalIndex] = tempNumber;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", numbers[i]);
+    }
     return 0;
 }
