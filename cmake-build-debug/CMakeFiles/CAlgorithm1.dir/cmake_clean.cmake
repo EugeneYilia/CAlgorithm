7e31d7c912629564834164886fe76cea8607b2ce
root <root@DESKTOP-NM1J0DE.localdomain>
2018-07-09 12:45:05 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/CAlgorithm1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CAlgorithm1.dir/main.c.obj"
+  "CAlgorithm1.pdb"
+  "CAlgorithm1.exe"
+  "CAlgorithm1.exe.manifest"
+  "libCAlgorithm1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CAlgorithm1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
